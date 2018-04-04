f2308a1b565b20950c4c77c02099e320cf593a26
Vlad Markov <markov@cs.petrsu.ru>
2018-04-04 15:03:28 +0300

    pp

--- tests/fibonachi_test.h
+++ tests/fibonachi_test.h
@@ -97,7 +97,7 @@ dup2(oldstdout,STDOUT);
 }
 
 
-TEST(protest, pp)
+TEST(protest, pp_first_line)
 {
      int fptr;
      int oldstdout;
@@ -129,7 +129,7 @@ TEST(protest, pp)
      readcount+=fread(buf1,1,512,t2);
         }
      FILE *d2;
-     d2 =fopen("testi/proverka_pp.txt","rw");
+     d2 =fopen("testi/pp_first_line.txt","rw");
      if(d2 == NULL){
             FAIL();
             return;
@@ -161,6 +161,69 @@ TEST(protest, pp)
 }
 }
 
+TEST(protest, pp_second_line)
+{
+     int fptr;
+     int oldstdout;
+     int i=0;
+     fptr = open("DUMMY.PIL",O_CREAT|O_RDWR,S_IREAD|S_IWRITE);
+     oldstdout = dup(STDOUT);
+     dup2(fptr,STDOUT);
+     close(fptr);
+
+     text txt = create_text();
+     load(txt,(char *)"testi/load.txt");
+     c_to_pos(txt,0,0);
+     pp(txt,(char *)"good");
+
+
+     show(txt);
+     dup2(oldstdout,STDOUT);
+
+     FILE *t2;
+     t2 = fopen("DUMMY.PIL","rw");
+     if(t2 == NULL){
+            FAIL();
+            return;
+     }
+     char *buf1 = (char*)malloc(sizeof(char)*512);
+
+     int readcount =  0;
+     while(!feof(t2)){
+     readcount+=fread(buf1,1,512,t2);
+        }
+     FILE *d2;
+     d2 =fopen("testi/pp_second_line.txt","rw");
+     if(d2 == NULL){
+            FAIL();
+            return;
+     }
+     char *buf2 = (char*)malloc(sizeof(char)*512);
+     int readcount2 = 0;
+     while(!feof(d2)){
+     readcount2+=fread(buf2,1,512,d2);
+        }
+   while(!feof(t2) && !feof(d2)){
+        /*char* k1 = fgets(buf1, 512, t2);
+        char* k2 = fgets(buf2, 512, d2);
+        if(k1==k2){*/
+         for(i=0;i<512;i++){
+             if(buf1[i] != buf2[i])
+                 FAIL();
+         //}
+         /**buf1 = "";
+         *buf2 = "";*/
+   }
+}
+
+
+     fclose(t2);
+     fclose(d2);
+     ASSERT_EQ(readcount,readcount2);
+     while(!feof(d2) && !feof(t2)){
+
+}
+}
 
 
 /*TEST(fibonachiTest, num0) {
