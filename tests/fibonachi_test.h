cb5727646fe1a06ddfe61d2897d727d60f377091
JoeRed <markov@cs.karelia.ru>
2018-04-11 15:02:44 +0300

    mplb2

--- tests/fibonachi_test.h
+++ tests/fibonachi_test.h
@@ -10,6 +10,7 @@ extern "C" {
 #include "text/text.h"
 #include "text/_text.h"
 #define STDOUT 1
+#define STDERR 2
 }
 TEST (abc, test1)
 {
@@ -241,6 +242,75 @@ TEST(protest, pp_second_line)
 }
 }
 
+TEST(protest, mplb_second_line)
+{
+     int fptr;
+     int oldstdout;
+     int i=0;
+     fptr = open("MPLBTEST2",O_CREAT|O_RDWR,S_IREAD|S_IWRITE);
+     oldstdout = dup(STDOUT);
+     dup2(fptr,STDOUT);
+     close(fptr);
+
+     char *filedir=(char*)malloc(1024);
+     sprintf(filedir, "%s/load.txt", TESTIDIR);
+
+     text txt = create_text();
+     load(txt, filedir);
+     c_to_pos(txt,1,1);
+     mplb(txt);
+
+     show(txt);
+     dup2(oldstdout,STDOUT);
+
+     FILE *t2;
+     t2 = fopen("MPLBTEST2","rw");
+     if(t2 == NULL){
+            FAIL();
+            return;
+     }
+     char *buf1 = (char*)malloc(sizeof(char)*512);
+
+     int readcount =  0;
+     while(!feof(t2)){
+     readcount+=fread(buf1,1,512,t2);
+        }
+     FILE *d2;
+
+     sprintf(filedir, "%s/mplb_second_line.txt", TESTIDIR);
+     d2 =fopen(filedir,"rw");
+
+     if(d2 == NULL){
+            FAIL();
+            return;
+     }
+     char *buf2 = (char*)malloc(sizeof(char)*512);
+     int readcount2 = 0;
+     while(!feof(d2)){
+     readcount2+=fread(buf2,1,512,d2);
+        }
+   /*while(!feof(t2) && !feof(d2)){
+        char* k1 = fgets(buf1, 512, t2);
+        char* k2 = fgets(buf2, 512, d2);
+        if(k1==k2){*/
+         for(i=0;i<512;i++){
+             if(buf1[i] != buf2[i])
+                 ASSERT_EQ(i,0);
+                 //FAIL();
+         //}
+         /**buf1 = "";
+         *buf2 = "";*/
+  // }
+}
+
+
+     fclose(t2);
+     fclose(d2);
+     ASSERT_EQ(readcount,readcount2);
+     while(!feof(d2) && !feof(t2)){
+
+}
+}
 
 /*TEST(fibonachiTest, num0) {
     ASSERT_EQ(fibonachi(0), 0);
